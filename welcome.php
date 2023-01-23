2153b8cc251703800bfcbb1dfca5d186838f69a4
piyumi wathsala <piyumiwathsala310@gmail.com>
2023-01-23 23:12:14 +0530

    welcome page changed

--- welcome.php
+++ welcome.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/welcome.css">
     <script src="https://kit.fontawesome.com/4801a7dc21.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
@@ -28,31 +29,79 @@
     
     </div>
 
-<!-- wallpaper part -->
-<div class="section">
+<!-- Slider -->
+<div class="slider">
+		<!-- fade css -->
+		<div class="myslide fade">
+			<div class="txt">
+				<h1 class="topic">Welcome to Food Donation Gallery</h1>
+				<p class="sub_topic">Whoever is happy will make others happy too.<br>Let’s grab healthy & delicious food to be wealthy in donations.</p>
+			</div>
+			<img src="img/img1.jpg"  style= "width: 100%; height: 100%; filter: brightness(50%);">
+		</div>
+		
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>Let’s join hands to feed the hungry.</h1>
+				<p>Alone we can do little; Together we  can do so much.<br>Let’s work hand in hand against hunger.</p>
+			</div>
+			<img src="img/img2.jpg" style="width: 100%; height: 100%; filter: brightness(50%);">
+		</div>
+				
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>Enjoy the best food with best taste</h1>
+				<p>Need delicious and healthy food to eat? <br> Welcome to our food gallery where you can get good food easily</p>
+			</div>
+			<img src="img/img4.jpg" style="width: 100%; height: 100%; filter: brightness(50%);">
+		</div>
+		
+    <div class="myslide fade">
+			<div class="txt">
+				<h1>Help us lift up young spirits</h1>
+				<p>Being generous today will build a hunger-free tomorrow</p>
+			</div>
+			<img src="img/img3.jpg" style="width: 100%; height: 100%; filter: brightness(50%);">
+		</div>
 
-    <h1>Welcome To Food Donation Gallery</h1>
+		<div class="myslide fade">
+			<div class="txt">
+				<h1>Healthy food for wealthy mood</h1>
+				<p>We give you an eating experience that you'll never forget</p>
+			</div>
+			<img src="img/img5.jpg" style="width: 100%; height: 100%; filter: brightness(40%);">
+		</div>
+		<!-- /fade css -->
+		
+		<!-- onclick js -->
+		<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  		<a class="next" onclick="plusSlides(1)">&#10095;</a>
+		
+		<div class="dotsbox" style="text-align:center">
+			<span class="dot" onclick="currentSlide(1)"></span>
+			<span class="dot" onclick="currentSlide(2)"></span>
+			<span class="dot" onclick="currentSlide(3)"></span>
+			<span class="dot" onclick="currentSlide(4)"></span>
+			<span class="dot" onclick="currentSlide(5)"></span>
+		</div>
+		<!-- /onclick js -->
+	</div>
+<!-- Slider -->
 
-    <div class="video-container">
-        <div class="color-overlay"></div>
-        <video autoplay loop muted>
-            <source src="home.mp4" type="video/mp4">
-        </video>
-    </div>
 
-</div>
+
+
+
 
 <!-- main content -->
-<div class="grid">
+  <div class="grid">
       <div class="grid-item">
         <div class="card">
           <img class="card-img" src="./img/donate.jpg" />
           <div class="card-content">
             <h1 class="card-header">Food & Money Donate</h1>
             <p class="card-text">
-              Rome is known for its stunning <strong> architecture</strong>,
-              with the Colleseum, Pantheon, and Trevi Fountain as the main
-              attractions.
+            Want to became a earth protector? This is the best option to protect life on earth by donating Foods or Money. Choose what you prefer. Lives are waiting for you!  
             </p>
           </div>
         </div>
@@ -64,10 +113,7 @@
           <div class="card-content">
             <h1 class="card-header">Food Order & Fund</h1>
             <p class="card-text">
-              One of the world's natural wonders, the iconic Grand Canyon draws
-              oohs and aahs from visitors perched at the edge of its
-              <strong>towering cliffs</strong>.
-            </p>
+            Busy schedule? Food donation Gallery is the best solution to ease your busy life. With one click on get your ordered foods to your.             </p>
           </div>
         </div>
       </div>
@@ -77,14 +123,12 @@
           <div class="card-content">
             <h1 class="card-header">Treatments</h1>
             <p class="card-text">
-              The Maldives are known for their
-              <strong>natural environment</strong> including the blue ocean,
-              white beaches, and clean air, attracting tourists.
-            </p>
+            Malnutrition is a critical problem in now a day. View best Recommended Treatments for prevent Malnutrition.            </p>
           </div>
         </div>
       </div>
     </div>
+
 
 
 <!-- about section -->
@@ -119,9 +163,7 @@
             </div>
             <a href="#" class="btn">Learn More</a>
         </div>
-
     </div>
-
 </section>
 
 <!-- footer -->
@@ -172,6 +214,8 @@
     
     
 <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+<script src="js/jsfile.js"></script>
+<script src="js/anim.js"></script>
 <script src="js/style.js"></script>
 
 </body>
